fefaedc6779382e89f026d5643a228e213842381
Giovanni-Ostemberg <gigio.ostemberg@gmail.com>
2020-07-27 14:03:33 -0400

    exercicio 1

--- funcao.js
+++ funcao.js
@@ -1,16 +1,44 @@
 import {promises as fs} from "fs";
 
-createStateJson();
+var cities = [];
+var states = [];
+start();
 
-async function createStateJson(){
-    const states = JSON.parse(await fs.readFile("Estados.json"));
-    console.log(states);
+async function start(){
+    cities = await createCitiesJson();
+    states = await createStateJson();
+    states.forEach(element => { prepareJson(cities, element) });
 
-    states.forEach(element => {
-         createJson(element);
-    });
 }
-async function createJson(element){
-    const path = './estados/' + element.Sigla + '.json';
-    await fs.writeFile(path, JSON.stringify(element));
+
+
+
+
+async function createStateJson(){ 
+
+    return await JSON.parse(await fs.readFile("Estados.json"));
 }
+
+async function createCitiesJson(){
+    return await JSON.parse(await fs.readFile("Cidades.json"));
+}
+
+async function prepareJson(cities, element){
+   
+        let citiesByState =  await filterCities(element);
+        const path = './estados/' + element.Sigla + '.json';
+        await createJson(path, citiesByState);
+
+}
+
+async function filterCities(element){
+    console.log(element);
+    return await cities.filter(city => {return city.Estado === element.ID;});
+
+}
+
+async function createJson(path, citiesByState){
+    //let citiesJson = await JSON.parse(citiesByState);
+    return await fs.writeFile(path, JSON.stringify(citiesByState));
+}
+
